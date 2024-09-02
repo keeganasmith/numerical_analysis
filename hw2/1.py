b0b16345cf7daab992c9ec36bf87e343656bc8cd
Keegan Smith <keeganasmith2003@gmail.com>
2024-09-02 15:27:19 -0700

    g2, etc

--- hw2/1.py
+++ hw2/1.py
@@ -1,25 +1,41 @@
 import math;
-NUM_ITER = 20
+NUM_ITER = 6
 def f(x):
     return math.e**x - x - 1;
 def f_prime(x):
     return math.e**x - 1;
+def f_double_prime(x):
+    return math.e**x;
 def g1(x):
     return x - f(x) / f_prime(x);
-def compute(initial_x, function):
-    result = ""
-    approx = initial_x
+def g2(x):
+    return x - (f(x)*f_prime(x)) / (f_prime(x)**2 - f(x) * f_double_prime(x))
+def compute(initial_x, function, f_function):
+    result = "\\begin{tabular}{|c|c|c|c|}\n"
+    result += "\\hline\n"
+    result += "Iteration & Approximation & Residual\\\\\n"
+    result += "\\hline\n"
+    curr_approx = initial_x
+    residual = f_function(curr_approx)
+    
     for i in range(0, NUM_ITER):
-        result += f"Iteration: {i}, approx: {approx}\n"
+        result += f"{i} & {curr_approx:.6e} & {residual:.6e}\\\\\n"
+        result += "\\hline\n"
+        curr_approx = function(curr_approx)
 
-        approx = function(approx)
+        residual = f_function(curr_approx)
+    result += "\\end{tabular}\n"
+
     return result
     #return approx;
 function_mapping = {
-    "f" : [f, f_prime]
+    "f" : f,
+    "g1": g1,
+    "g2": g2
 }
 if __name__ == "__main__":
     initial_num = float(input("enter initial num: "))
-    my_func = input("enter the name of the function: ")
-    result = compute(initial_num, function_mapping[my_func][0],function_mapping[my_func][1])
+    my_func = input("enter the name of the function (g): ")
+    my_func_2 = input("enter the name of the function (f): ")
+    result = compute(initial_num, function_mapping[my_func], function_mapping[my_func_2])
     print(result)
